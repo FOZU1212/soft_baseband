878c9e6c4b750565a5002ef5b428ac4203ebc9d1
yujiamanong <1392468976@qq.com>
2017-04-18 13:43:49 +0800

    Add files via upload

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "./commonStructure/commonStructure.h"
+#include "./globalVarINIT/globalVarINIT.h"
+#include "./IFFT/ifft.h"
+#include "./integerTypeDef/integerTypeDef.h"
+#include "./process/process.h"
+#include "./process_data/process_data.h"
+
+#endif // ALLHEADERS
